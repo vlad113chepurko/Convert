93d126cfedd3104f39d1a873596a905792bb506f
Vlad Chepurko <vlad113chepurko@gmail.com>
2025-02-26 10:04:44 +0200

    Added convert to oct

--- index.py
+++ index.py
@@ -9,13 +9,18 @@ class Convert:
     print("2. Octal")
     print("3. Hexadecimal")
     self.choise = input("Enter the type you want to convert to: ")
+
     if(self.choise == 1):
       return self.convert_to_binary()
+    elif(self.choise == 2):
+      return self.convert_to_octal()
 
 
   def convert_to_binary(self):
     return bin(self.value)
-    
+  
+  def convert_to_octal(self):
+    return oct(self.value)
 
 value = int(input("Enter the value you want to convert: "))
 convert = Convert(value, 1)
