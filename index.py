f08905efc1940da48f7148b21d44e4bb3b6ff9f4
Vlad Chepurko <vlad113chepurko@gmail.com>
2025-02-26 10:08:08 +0200

    Defeat problems

--- index.py
+++ index.py
@@ -1,20 +1,19 @@
 class Convert:
-  def __init__(self, value, choise):
+  def __init__(self, value):
     self.value = value
-    self.choise = choise
 
   def logic(self):
     print("You can convert the value to other types")
     print("1. Binary")
     print("2. Octal")
     print("3. Hexadecimal")
-    self.choise = input("Enter the type you want to convert to: ")
+    choise = int(input("Enter the type you want to convert to: "))
 
-    if(self.choise == 1):
+    if(choise == 1):
       return self.convert_to_binary()
-    elif(self.choise == 2):
+    elif(choise == 2):
       return self.convert_to_octal()
-    elif(self.choise == 3):
+    elif(choise == 3):
       return self.convert_to_hexadecimal()
 
 
@@ -28,5 +27,5 @@ class Convert:
     return hex(self.value)
 
 value = int(input("Enter the value you want to convert: "))
-convert = Convert(value, 1)
+convert = Convert(value)
 print(convert.logic())
